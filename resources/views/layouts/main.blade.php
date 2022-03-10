abf36ef058b695190574830ff26d90dc7bcbf45e
naeemliaqatweb <naeemliaqatweb@gmail.com>
2022-03-10 17:28:19 +0500

    ccx

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -278,7 +278,7 @@
     <div class="modal-dialog modal-dialog-centered" role="document">
       <div class="modal-content">
         <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLongTitle">Verify your human</h5>
+          <h5 class="modal-title text-dark" id="exampleModalLongTitle">Verify your human</h5>
           <button type="button" class="close" data-dismiss="modal" aria-label="Close">
             <span aria-hidden="true">×</span>
           </button>
