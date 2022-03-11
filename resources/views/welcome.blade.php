c4391f9cfa29a0eff59f0d3ce178fb6ce1dd4abc
naeemliaqatweb <naeemliaqatweb@gmail.com>
2022-03-11 13:07:56 +0500

    dds

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -313,7 +313,7 @@ if it's not present, don't show loader */
                             @endphp
                             <tr>
 
-                                <td>{{ $row_per->id}}</td>
+                                <td>{{ $i++}}</td>
                                 <td >
                                    
                                          <a href="{{url('coins', ['id'=>$row_per->id])}}" class="name py-2"><img src="{{$row_per->logo_link}}" class="">{{$row_per->name}}</a>
